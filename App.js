573332b185e5690782417a6a2914af80dcb4c7e4
Takayuki Komatsu <komatsutakayuki235@live.com>
2020-08-22 20:53:50 -0300

    Added a version using Hooks

--- App.js
+++ App.js
@@ -1,76 +1,66 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 import { Button, TextInput } from "react-native-paper";
-export default class App extends Component {
-  state = {
-    peso: 0,
-    altura: 0,
-    imc: 0,
-    legenda: "Indeterminado",
-    cor: "#bfc3c7",
-  };
 
-  calcularIMC = () => {
-    const resultado = this.state.peso / (this.state.altura * this.state.altura);
+export default function App() {
+  const [peso, setPeso] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [imc, setIMC] = useState(0);
+  const [legenda, setLegenda] = useState("");
+  const [cor, setCor] = useState("");
 
-    this.setState({
-      imc: Math.ceil(resultado),
-    });
+  function calcularIMC() {
+    const resultado = peso / (altura * altura);
+    setIMC(Math.ceil(resultado));
 
     if (resultado < 18.5) {
-      this.setState({ legenda: "Magreza", cor: "#e74c3c" });
+      setLegenda("Magreza");
+      setCor("#e74c3c");
     } else if (resultado >= 18.5 && resultado < 25) {
-      this.setState({ legenda: "Normal", cor: "##2ecc71" });
+      setLegenda("Normal");
+      setCor("##2ecc71");
     } else if (resultado >= 25 && resultado < 30) {
-      this.setState({ legenda: "Sobrepeso", cor: "#f1c40f" });
+      setLegenda("Sobrepeso");
+      setCor("#f1c40f");
     } else if (resultado >= 30 && resultado < 40) {
-      this.setState({ legenda: "Obesidade", cor: "#e67e22" });
+      setLegenda("Obesidade");
+      setCor("#e67e22");
     } else if (resultado >= 40) {
-      this.setState({ legenda: "Obesidade grave", cor: "#e74c3c" });
+      setLegenda("Obesidade grave");
+      setCor("#e74c3c");
     }
-  };
-
-  render() {
-    return (
-      <View style={styles.app}>
-        <Text style={styles.legenda}>Seu IMC</Text>
-
-        <View style={[{ backgroundColor: this.state.cor }, styles.painel]}>
-          <Text style={styles.resultado}>{this.state.imc}</Text>
-          <Text style={styles.diagnostico}>{this.state.legenda}</Text>
-        </View>
-
-        <View>
-          <TextInput
-            style={styles.peso}
-            label="Peso"
-            keyboardType="number-pad"
-            onChangeText={(value) => {
-              this.setState({
-                peso: value.replace(",", "."),
-              });
-            }}
-          />
-
-          <TextInput
-            style={styles.altura}
-            label="Altura"
-            keyboardType="number-pad"
-            onChangeText={(value) => {
-              this.setState({
-                altura: value.replace(",", "."),
-              });
-            }}
-          />
-
-          <Button mode="contained" onPress={this.calcularIMC}>
-            Calcular
-          </Button>
-        </View>
-      </View>
-    );
   }
+
+  return (
+    <View style={styles.app}>
+      <Text style={styles.legenda}>Seu IMC</Text>
+
+      <View style={[{ backgroundColor: cor }, styles.painel]}>
+        <Text style={styles.resultado}>{imc}</Text>
+        <Text style={styles.diagnostico}>{legenda}</Text>
+      </View>
+
+      <View>
+        <TextInput
+          style={styles.peso}
+          label="Peso"
+          keyboardType="number-pad"
+          onChangeText={(value) => setPeso(value.replace(",", "."))}
+        />
+        <TextInput
+          style={styles.altura}
+          label="Altura"
+          keyboardType="number-pad"
+          onChangeText={(value) => setAltura(value.replace(",", "."))}
+        />
+
+        <Button mode="container" onPress={calcularIMC}>
+          Calcular
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
